a93753ce71ce387e0ede22d11f73b929b5ac871d
Ng Ting Fung <ntf@ntflab.com>
2014-03-24 20:19:22 +0800

    fix Pyroblast animation and position

--- game/contents/spells/Pyroblast.js
+++ game/contents/spells/Pyroblast.js
@@ -15,9 +15,9 @@ Pyroblast = function() {
 		},
 		"animations" : {
 			"initial" : [ 0, 4, "travel", 2 ],
-			"travel" : [ 5 , 14, "travel", 2],
+			"travel" : [ 5, 14, "travel", 2 ],
 			"explosion" : [ 15, 19, "end", 4 ],
-			"end" : [ 20, 24 , 2]
+			"end" : [ 20, 24, null, 4 ]
 		}
 	}));
 	this.defaultAnimation = "initial";
@@ -27,19 +27,20 @@ Pyroblast = function() {
 
 util.inherits(Pyroblast, Castable);
 
-Pyroblast.prototype.m1 = function() {	
+Pyroblast.prototype.m1 = function() {
 	return this;
 };
 
 Pyroblast.prototype.execute = function(caster) {
 	console.log("Pyroblast");
-	this.sprite.setTransform(caster.sprite.x+120, caster.sprite.y-20, 1, 1);
+	this.sprite.setTransform(caster.sprite.x, caster.sprite.y + 70, 1, 1);
 	this.position.vertical = (caster.position.vertical + 1);
 	this.position.horizontal = caster.position.horizontal;
 	var level = this.world.activeLevel;
 	level.add(this);
 	this.gotoAndPlay("initial");
-//	done();
-	//var tween = createjs.Tween.get(this).to({x:caster.boardWidth, y:caster.y}, (600 * distance) , createjs.Ease.linear);
+	// done();
+	// var tween = createjs.Tween.get(this).to({x:caster.boardWidth,
+	// y:caster.y}, (600 * distance) , createjs.Ease.linear);
 	// explosion remember to transform
 };
