4d126bddb31c79b96a5e274ed7026f58cdd74141
longyatmok <longyatmok@gmail.com>
2014-05-09 14:41:32 +0800

    Thunderbolt calTarget

--- game/contents/spells/Thunderbolt.js
+++ game/contents/spells/Thunderbolt.js
@@ -2,63 +2,73 @@ var util = require('util');
 var Castable = require('../../gameobjects/Castable');
 
 var Thunderbolt = function(world, args) {
-	Thunderbolt.super_.call(this, world);
-	this.name = "Thunderbolt";
-	this.setSpriteSheet(new createjs.SpriteSheet({
-		"images" : [ this.world.assets.getResult("thunderbolt") ],
-		"frames" : {
-			"regX" : 160,
-			"height" : 560,
-			"count" : 39,
-			"regY" : 0,
-			"width" : 320
-		},
-		"animations" : {
-			"initial" : [ 0, 39, null, 10 ]
-		}
-	}));
-	this.defaultAnimation = "initial";
-	this.args = args;
-	this.castTime = 5;
-	console.log("Thunderbolt initialized");
-	this.initialize();
+    Thunderbolt.super_.call(this, world);
+    this.name = "Thunderbolt";
+    this.setSpriteSheet(new createjs.SpriteSheet({
+        "images": [this.world.assets.getResult("thunderbolt")],
+        "frames": {
+            "regX": 160,
+            "height": 560,
+            "count": 39,
+            "regY": 0,
+            "width": 320
+        },
+        "animations": {
+            "initial": [0, 39, null, 10]
+        }
+    }));
+    this.defaultAnimation = "initial";
+    this.args = args;
+    this.castTime = 5;
+    console.log("Thunderbolt initialized");
+    this.initialize();
 }
 
 util.inherits(Thunderbolt, Castable);
 
 Thunderbolt.prototype.execute = function(caster, onComplete) {
-	return (function() {
-		this.queue_(function(done) {
-			console.log("Thunderbolt cast");
-			var distX = parseInt(this.args[0]);
-			var distY = parseInt(this.args[1]);
-			// do the range limit
-			if ((distX > 0 && distX <= caster.stats.castRange)
-					&& (distY > 0 && distY <= caster.stats.castRange)) {
-				this.targetX = caster.position.horizontal + distX;
-				this.targetY = caster.position.vertical + distY;
-				if (this.targetX < 12 && this.targetY < 6) {
-					this.sprite.setTransform(caster.sprite.x + (120 * distX),
-							caster.sprite.y - 480 + (120 * distY), 1, 1);
-					this.position.vertical = this.targetY;
-					this.position.horizontal = this.targetX;
-					var level = this.world.activeLevel;
-					level.add(this);
-					this.gotoAndPlay("initial", done);
-				} else {
-					console.log("Casting Range exist board range");
-				}
-			} else {
-				console.log("Casting Range exist character's range");
-			}
-		});
-		this.queue_(function(done) {
-			console.log("Thunderbolt cast finish");
-			var level = this.world.activeLevel;
-			level.remove(this);
-			onComplete();
-		});
-		this.triggerAction();
-	}).bind(this);
+    return (function() {
+        this.queue_(function(done) {
+            console.log("Thunderbolt cast");
+            // calculate target
+            this.calTarget(caster);
+            var distX = this.targetX - caster.position.horizontal;
+            var distY = this.targetY - caster.position.vertical;
+            this.sprite.setTransform(caster.sprite.x + (120 * distX),
+                    caster.sprite.y - 480 + (120 * distY), 1, 1);
+            this.position.vertical = this.targetY;
+            this.position.horizontal = this.targetX;
+            var level = this.world.activeLevel;
+            level.add(this);
+            this.gotoAndPlay("initial", done);
+
+        });
+        this.queue_(function(done) {
+            console.log("Thunderbolt cast finish");
+            var level = this.world.activeLevel;
+            level.remove(this);
+            onComplete();
+        });
+        this.triggerAction();
+    }).bind(this);
+};
+
+Thunderbolt.prototype.calTarget = function(caster) {
+    console.log(this.name + " calculating target");
+    var gameBoard = this.world.activeLevel.gameboard;
+    var distX = parseInt(this.args[0]);
+    var distY = parseInt(this.args[1]);
+    // do the range limit
+    if ((distX > 0 && distX <= caster.stats.castRange)
+            && (distY > 0 && distY <= caster.stats.castRange)) {
+        this.targetX = caster.position.horizontal + distX;
+        this.targetY = caster.position.vertical + distY;
+    } else {
+        throw new Error("Cast Distance too long");
+    }
+    if (this.targetX < 12 || this.targetY < 6) {
+        throw new Error("Spells casted in VOID!");
+    }
+    this.target = gameBoard[this.targetX][this.targetY];
 };
 module.exports = Thunderbolt;
--- game/gameobjects/Castable.js
+++ game/gameobjects/Castable.js
@@ -2,38 +2,39 @@ var util = require('util');
 var BoardObject = require('./BoardObject');
 var core = require('../client/Core');
 var Castable = function(world) {
-	console.log(core);
-	Castable.super_.call(this, world);
-	this.name = "(Castable Spell)";
-	this.castTime = 1;
-        // require calculation of target
-        this.targetX = null;
-        this.targetY = null;
-        this.target = [];
+    console.log(core);
+    Castable.super_.call(this, world);
+    this.name = "(Castable Spell)";
+    this.castTime = 1;
+    // require calculation of target
+    this.targetX = null;
+    this.targetY = null;
+    this.target = [];
 };
 
 util.inherits(Castable, BoardObject);
 
 
-Castable.prototype.calTarget = function(){
+Castable.prototype.calTarget = function() {
     // require override in child class.
-    /**
+    /*
      Done class:
-        Fireball - 
-        Firepillar - args check, bound check, register on board
-        Thunderbolt
-        
+     Fireball    - OK, calTarget put list of enemy
+     Firepillar  - OK, args check, bound check, register on board,  TODO: remember to add checking in enemy move
+     Thunderbolt - OK,                                              TODO: calTarget
+     Lightwall   - OK                                               TODO: remember to add checking in enemy move
+     
      */
-    
+
 };
 
 Castable.prototype.execute = function(caster, onComplete) {
-	if (caster instanceof BoardObject) {
-		console.log(caster + " casts " + this.name);
-		onComplete();
-	} else {
-		throw new Error(Caster + " is not a BoardObject");
-	}
+    if (caster instanceof BoardObject) {
+        console.log(caster + " casts " + this.name);
+        onComplete();
+    } else {
+        throw new Error(Caster + " is not a BoardObject");
+    }
 };
 Castable.DummyCastable = function() {
 
