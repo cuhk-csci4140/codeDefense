abe750d405ebee15cbb1d3f112b71d56cd68b9ea
Ng Ting Fung <ntf@ntflab.com>
2014-05-10 15:41:58 +0800

    Fix lightwall , add maxMp to player

--- game/contents/mobs/Chibi.js
+++ game/contents/mobs/Chibi.js
@@ -6,94 +6,101 @@ var CombatService = require('../services/CombatService');
 var TestPlayer = require('../../gameobjects/TestPlayer');
 
 var Chibi = function(world) {
-    Chibi.super_.call(this, world);
-    this.hp = 5;
-    this.originHp = 5;
-    this.setSpriteSheet(new createjs.SpriteSheet({
-        "images": [world.assets.getResult("chibi")],
-        "frames": {
-            "regX": 108,
-            "height": 216,
-            "count": 35,
-            "regY": 108,
-            "width": 216
-        },
-        "animations": {
-            "stand": [31, 34, "stand"],
-            "initRun": [6, 9, "run", 6],
-            "run": [6, 9, "run", 4],
-            "attack": [10, 22, "stand", 5]
-        }
-    }));
-    this.defaults.movement = 2000;
-    this.stats.movement = 2000;
-    this.defaultAnimation = "stand";
-    this.damage = 50;
+	Chibi.super_.call(this, world);
+	this.hp = 5;
+	this.originHp = 5;
+	this.setSpriteSheet(new createjs.SpriteSheet({
+		"images" : [ world.assets.getResult("chibi") ],
+		"frames" : {
+			"regX" : 108,
+			"height" : 216,
+			"count" : 35,
+			"regY" : 108,
+			"width" : 216
+		},
+		"animations" : {
+			"stand" : [ 31, 34, "stand" ],
+			"initRun" : [ 6, 9, "run", 6 ],
+			"run" : [ 6, 9, "run", 4 ],
+			"attack" : [ 10, 22, "stand", 5 ]
+		}
+	}));
+	this.defaults.movement = 2000;
+	this.stats.movement = 2000;
+	this.defaultAnimation = "stand";
+	this.damage = 50;
+	this.hp = 3;
+	this.myTurn = {};
 
-    this.myTurn = {};
+	this.AI = function(event) {
 
-    this.AI = function(event) {
-        if (event.turn == this.faction) {
-            this.myTurn = event;
+		if (event.turn == this.faction) {
+			this.myTurn = event;
 
-            // action 1
-            if (this.position.horizontal > 1) {
-                var grid = this.world.activeLevel.gameboard[this.position.horizontal - 1][this.position.vertical];
-                if (grid == null) {
-                    // able to move
-                    this.move(-1, 0);
-                } else {
-                    if (grid instanceof Firepillar) {
-                        this.move(-1, 0);
-                        grid.dealDamage();
-                    } else if (grid instanceof Lightwall) {
-                        // dun move, no codes
-                    } else if (grid instanceof TestPlayer) {
-                        // attack the player!
-                        this.attack();
-                        grid.hp -= this.damage;
-                    }
-                }
-            } else if (this.position.horizontal == 1) {
-                this.move(-1, 0);
-                this.world.gameobjects.get("player").hp -= this.damage;
-                // showBox("CAUTION!", "A monster just passed you.");
-            } else {
-                // despawn
-                this.dispose();
-            }
+			// action 1
+			if (this.position.horizontal > 1) {
+				var grid = this.world.activeLevel.gameboard[this.position.horizontal - 1][this.position.vertical];
+				if (grid == null) {
 
-            // action 2
-            this.queue_(function(done) {
+					var grid2 = this.world.activeLevel.gameboard[this.position.horizontal - 2][this.position.vertical];
+					// able to move
+					if (grid2 == null) {
+						this.move(-2, 0);
+					} else {
+						this.move(-1, 0);
+					}
+				} else {
+					if (grid instanceof Firepillar) {
+						this.move(-1, 0);
+						grid.dealDamage();
+					} else if (grid instanceof Lightwall) {
+						// dun move, no codes
+					} else if (grid instanceof TestPlayer) {
+						// attack the player!
+						this.attack();
+						grid.hp -= this.damage;
+					}
+				}
+			} else if (this.position.horizontal == 1) {
+				this.move(-1, 0);
+				this.world.gameobjects.get("player").hp -= this.damage;
+				// showBox("CAUTION!", "A monster just passed you.");
+			} else {
+				// despawn
+				this.dispose();
+			}
 
-                done();
-                world.services[CombatService.NAME].nextTurn(event);
-                console.log("Chibi is done");
-            });
+			// action 2
+			this.queue_(function(done) {
 
-            // start the queue.
-            this.triggerAction();
-        }
-    }.bind(this);
-    // register to script service
-    world.services[CombatService.NAME].subscribe(CombatService.Events.NextTurn,
-            this.AI);
+				done();
+				world.services[CombatService.NAME].nextTurn(event);
+				console.log("Chibi is done");
+			});
 
-    console.log("Chibi initialized");
-    this.initialize();
+			// start the queue.
+			this.triggerAction();
+		}
+	}.bind(this);
+	// register to script service
+	world.services[CombatService.NAME].subscribe(CombatService.Events.NextTurn,
+			this.AI);
+
+	console.log("Chibi initialized");
+	this.initialize();
 };
 
 util.inherits(Chibi, Mob);
 
 Chibi.prototype.onStartMoving_ = function() {
-    this.gotoAndPlay("initRun");
+	this.gotoAndPlay("initRun");
 };
 
 Chibi.prototype.onStopMoving_ = function() {
-    this.gotoAndPlay("stand");
+	this.gotoAndPlay("stand");
 };
-Chibi.prototype.attack = function(){
-    this.gotoAndPlay("attack");
+Chibi.prototype.attack = function() {
+	this.gotoAndPlay("attack");
 }
 // BoardObject.prototype.moveTo_ = function(h, v, equation, onCompleteEvent) {
 module.exports = Chibi;
--- game/contents/spells/Lightwall.js
+++ game/contents/spells/Lightwall.js
@@ -1,64 +1,82 @@
 var util = require('util');
 var Castable = require('../../gameobjects/Castable');
-
+var CombatService = require('../services/CombatService');
 var Lightwall = function(world, args) {
-    Lightwall.super_.call(this, world);
-    this.name = "Lightwall";
-    this.setSpriteSheet(new createjs.SpriteSheet({
-        "images": [this.world.assets.getResult("lightwall")],
-        "frames": {
-            "regX": 98,
-            "height": 196,
-            "count": 27,
-            "regY": 96,
-            "width": 192
-        },
-        "animations": {
-            "initial": [0, 7, "active", 2],
-            "active": [8, 18, "active", 4],
-            "end": [19, 27, 2]
-        }
-    }));
-    this.defaultAnimation = "initial";
-    this.castTime = 1;
-    this.cost = 25;
-    if (args == null) {
-        throw new Error("No distance defined");
-    } else {
-        this.args = args;
-    }
-    console.log("Lightwall initialized");
-    this.initialize();
+	Lightwall.super_.call(this, world);
+	this.name = "Lightwall";
+	this.setSpriteSheet(new createjs.SpriteSheet({
+		"images" : [ this.world.assets.getResult("lightwall") ],
+		"frames" : {
+			"regX" : 98,
+			"height" : 196,
+			"count" : 27,
+			"regY" : 96,
+			"width" : 192
+		},
+		"animations" : {
+			"initial" : [ 0, 7, "active", 2 ],
+			"active" : [ 8, 18, "active", 4 ],
+			"end" : [ 19, 27, 2 ]
+		}
+	}));
+	this.defaultAnimation = "initial";
+	this.castTime = 1;
+	this.cost = 25;
+	if (args == null) {
+		throw new Error("No distance defined");
+	} else {
+		this.args = args;
+	}
+	console.log("Lightwall initialized");
+	this.initialize();
 }
 
 util.inherits(Lightwall, Castable);
 
 Lightwall.prototype.execute = function(caster, onComplete) {
-    this.queue_(function(done) {
-        console.log("lightwall");
-        if (this.args[0] > caster.stats.castRange) {
-            throw new Error("Cast Distance too long!");
-        }
-        var distX = this.args[0];
-        // check bounding, if exceed 12 cast at 12;        // check bounding, if exceed 12 cast at 12;
-        if ((caster.position.horizontal + distX) > 12) {
-            distX = 12 - caster.position.horizontal;
-        }
-        this.sprite.setTransform(caster.sprite.x + (120 * distX), caster.sprite.y - 20,
-                0.8, 0.8);
-        this.position.vertical = caster.position.vertical;
-        this.position.horizontal = (caster.position.horizontal + distX);
-        var level = this.world.activeLevel;
-        level.gameboard[this.position.horizontal][this.position.vertical] = this;
-        level.add(this);
-        this.gotoAndPlay("initial");
-        done();
+	this
+			.queue_(function(done) {
+				console.log("lightwall");
+				if (this.args[0] > caster.stats.castRange) {
+					throw new Error("Cast Distance too long!");
+				}
+				var distX = this.args[0];
+				// check bounding, if exceed 12 cast at 12; // check bounding,
+				// if exceed 12 cast at 12;
+				if ((caster.position.horizontal + distX) > 12) {
+					distX = 12 - caster.position.horizontal;
+				}
+				this.sprite.setTransform(caster.sprite.x + (120 * distX),
+						caster.sprite.y - 20, 0.8, 0.8);
+				this.position.vertical = caster.position.vertical;
+				this.position.horizontal = (caster.position.horizontal + distX);
+				var level = this.world.activeLevel;
+				level.gameboard[this.position.horizontal][this.position.vertical] = this;
+				level.add(this);
+				this.gotoAndPlay("initial");
+				done();
 
-    });
-    this.triggerAction();
-    // var tween = createjs.Tween.get(this).to({x:caster.boardWidth,
-    // y:caster.y}, (600 * distance) , createjs.Ease.linear);
+			});
+	this.triggerAction();
 
+	this.world.services[CombatService.NAME].callLater(5, (function() {
+		this.finish();
+	}).bind(this));
+
+};
+
+Lightwall.prototype.finish = function() {
+	this.queue_(function(done) {
+		// play the end motion
+		this.gotoAndPlay("end", done);
+	});
+
+	this.queue_(function(done) {
+		console.log("Lightwall destroy");
+		var level = this.world.activeLevel;
+		level.remove(this);
+	});
+	this.triggerAction();
 };
 
 module.exports = Lightwall;
