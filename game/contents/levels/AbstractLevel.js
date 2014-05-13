45eed084728619c31525cbe1f8d511e795d42b58
longyatmok <longyatmok@gmail.com>
2014-05-13 16:22:02 +0800

    Fix shooting spell, change win scoring

--- game/contents/levels/AbstractLevel.js
+++ game/contents/levels/AbstractLevel.js
@@ -4,27 +4,29 @@ var Mob = require('../mobs/Mob');
 var TestPlayer = require('../../gameobjects/TestPlayer');
 
 var AbstractLevel = function(world) {
-	this.world = world;
-	this.initialized = false;
-	this.stage = world.stage;
-	this.gameobjects = world.gameobjects;
-	// gameBoard for the board, init all null;
-	var array = new Array();
-	for (var i = 0; i < 12; i++) {
-		array[i] = new Array();
-		for (var j = 0; j < 6; j++) {
-			array[i][j] = null;
-		}
-	}
-	this.gameboard = array;
+    this.world = world;
+    this.initialized = false;
+    this.stage = world.stage;
+    this.gameobjects = world.gameobjects;
+    // gameBoard for the board, init all null;
+    var array = new Array();
+    for (var i = 0; i < 12; i++) {
+        array[i] = new Array();
+        for (var j = 0; j < 6; j++) {
+            array[i][j] = null;
+        }
+    }
+    this.gameboard = array;
+    this.baseTurn = 10;
+    this.baseCast = 6;
 };
 
-AbstractLevel.prototype.checkBoard = function(){
-    var i,j;
-    for(i=0;i<12;i++){
-        for(j=0;j<6;j++){
+AbstractLevel.prototype.checkBoard = function() {
+    var i, j;
+    for (i = 0; i < 12; i++) {
+        for (j = 0; j < 6; j++) {
             var target = this.gameboard[i][j];
-            if((target != null)&& !(target instanceof TestPlayer)){
+            if ((target != null) && !(target instanceof TestPlayer)) {
                 return false;
             }
         }
@@ -33,89 +35,108 @@ AbstractLevel.prototype.checkBoard = function(){
 };
 
 AbstractLevel.prototype.newGameObject = function(className) {
-	return new className(this.world);
+    return new className(this.world);
 };
 AbstractLevel.prototype.add = function() {
-	for (var i = 0; i < arguments.length; i++) {
-		this.add_(arguments[i]);
-	}
+    for (var i = 0; i < arguments.length; i++) {
+        this.add_(arguments[i]);
+    }
 
 };
 AbstractLevel.prototype.add_ = function(object) {
-        var stageChild = object;
-        var hpbar = null;
-	if (object instanceof GameObject) {
-		stageChild = object.getSprite();
-                if(object instanceof Mob){
-                    hpbar = object.getHPbar();
-                }
-	} else if (typeof object == "string") {
-		var temp = this.get(object);
-		if (!(temp instanceof GameObject)) {
-			throw new Error("gameobject:" + object
-					+ " is not a instanceof GameObject");
-		}
-		stageChild = temp.getSprite();
-	}
-        
-	this.world.stage.addChild(stageChild);
-        if(hpbar !== null){
-            this.world.stage.addChild(hpbar);
+    var stageChild = object;
+    var hpbar = null;
+    if (object instanceof GameObject) {
+        stageChild = object.getSprite();
+        if (object instanceof Mob) {
+            hpbar = object.getHPbar();
         }
+    } else if (typeof object == "string") {
+        var temp = this.get(object);
+        if (!(temp instanceof GameObject)) {
+            throw new Error("gameobject:" + object
+                    + " is not a instanceof GameObject");
+        }
+        stageChild = temp.getSprite();
+    }
+
+    this.world.stage.addChild(stageChild);
+    if (hpbar !== null) {
+        this.world.stage.addChild(hpbar);
+    }
 };
 AbstractLevel.prototype.remove = function() {
-	for (var i = 0; i < arguments.length; i++) {
-		this.remove_(arguments[i]);
-	}
+    for (var i = 0; i < arguments.length; i++) {
+        this.remove_(arguments[i]);
+    }
 
 };
 AbstractLevel.prototype.remove_ = function(object) {
-	var sprite;
-        var hpBar;
-	if (object instanceof GameObject) {
-		sprite = object.getSprite();
-                if(object instanceof Mob){
-                    hpBar = object.getHPbar();
-                }
-	} else if (typeof object == "string") {
-		var temp = this.get(object);
-		if (!(temp instanceof GameObject)) {
-			throw new Error("gameobject:" + object
-					+ " is not a instanceof GameObject");
-		}
-		object = temp;
-		sprite = temp.getSprite();
-	}
-	object.dispose();
-	this.world.stage.removeChild(sprite);
-        this.world.stage.removeChild(hpBar);
-	delete object;
+    var sprite;
+    var hpBar;
+    if (object instanceof GameObject) {
+        sprite = object.getSprite();
+        if (object instanceof Mob) {
+            hpBar = object.getHPbar();
+        }
+    } else if (typeof object == "string") {
+        var temp = this.get(object);
+        if (!(temp instanceof GameObject)) {
+            throw new Error("gameobject:" + object
+                    + " is not a instanceof GameObject");
+        }
+        object = temp;
+        sprite = temp.getSprite();
+    }
+    object.dispose();
+    this.world.stage.removeChild(sprite);
+    this.world.stage.removeChild(hpBar);
+    delete object;
 };
 AbstractLevel.prototype.set = function(name, object) {
-	if (typeof object == "function") {
-		object = this.newGameObject(object);
-	}
+    if (typeof object == "function") {
+        object = this.newGameObject(object);
+    }
 
-	if (object instanceof GameObject) {
-		this.gameobjects.add(name, object);
+    if (object instanceof GameObject) {
+        this.gameobjects.add(name, object);
 
-	} else {
-		throw new Error(object + " is not a instanceof GameObject");
-	}
+    } else {
+        throw new Error(object + " is not a instanceof GameObject");
+    }
 };
 AbstractLevel.prototype.get = function(name) {
-	return this.gameobjects.get(name);
+    return this.gameobjects.get(name);
 };
 AbstractLevel.prototype.initialize = function() {
-	this.initialized = true;
+    this.initialized = true;
 };
 
 AbstractLevel.prototype.update = function(event) {
-	if (this.initialized) {
+    if (this.initialized) {
 
-	}
+    }
 };
 AbstractLevel.prototype.dispose = function() {
 
+};
+
+AbstractLevel.prototype.win = function() {
+    showBox("WIN!", "GG");
+    var turn = world.services[ScriptService.NAME].turnCounter;
+    var hp = this.get('player').hp;
+    var mp = this.get('player').mp;
+    var cast = this.get('player').actionCounter;
+    if ((this.baseTurn - turn) <= 0) {
+        turn = 0;
+    }
+    if ((this.baseCast - cast) <= 0) {
+        cast = 0;
+    }
+    world.score += (this.baseTurn - turn) * 100 + hp + mp + (this.baseCast - cast) * 100;
+};
+
+AbstractLevel.prototype.jumpLevel = function(){
+    // override in each level to select new stage / end
 };
 module.exports = AbstractLevel;
--- game/contents/levels/testLevel.js
+++ game/contents/levels/testLevel.js
@@ -5,52 +5,56 @@ var Bori = require('../mobs/Bori');
 var CombatService = require('../services/CombatService');
 
 var testLevel = function(world) {
-	testLevel.super_.call(this, world);
+    testLevel.super_.call(this, world);
 };
 
 util.inherits(testLevel, AbstractLevel);
 
-testLevel.prototype.initialize = function() {
+testLevel.prototype.initialize = function(x, y) {
 
-	this.initialized = true;
+    this.initialized = true;
 
-	this.set('player', Mage);
-	this.get('player').sprite.setTransform(70, 60, 0.6, 0.6);
-	this.get('player').setPosition(5, 3);
-	this.get('player').setFaction(CombatService.TurnAlly);
+    this.set('player', Mage);
+    this.get('player').sprite.setTransform(70, 60, 0.6, 0.6);
+    if (x != null && y != null) {
+        this.get('player').setPosition(x, y);
+    } else {
+        this.get('player').setPosition(0, 0);
+    }
+    this.get('player').setFaction(CombatService.TurnAlly);
 
-	var ground = new createjs.Shape();
-	ground.graphics.beginBitmapFill(this.world.assets.getResult("board"),
-			"no-repeat").drawRect(0, 0, 1440, 727);
-	// ground.cache(0, 0, 1440, 727, 1 / world.ratio);
+    var ground = new createjs.Shape();
+    ground.graphics.beginBitmapFill(this.world.assets.getResult("board"),
+            "no-repeat").drawRect(0, 0, 1440, 727);
+    // ground.cache(0, 0, 1440, 727, 1 / world.ratio);
 
-	this.add(ground, this.get('player'));
+    this.add(ground, this.get('player'));
 
-	for (var i = 1; i <= 10; i++) {
-		this.set('bori' + i, Bori);
-		var currentBori = this.get('bori' + i);
-		currentBori.sprite.setTransform(0, 0, -0.6, 0.6);
-		var x;
-		var y;
-		do {
-			x = 4 + Math.ceil(Math.random() * 8) - 1;
-			y = Math.ceil(Math.random() * 6) - 1;
-		} while ((x == 5 && y == 3 && this.gameboard[x][y] != null));
-		// console.log(x);
+    for (var i = 1; i <= 10; i++) {
+        this.set('bori' + i, Bori);
+        var currentBori = this.get('bori' + i);
+        currentBori.sprite.setTransform(0, 0, -0.6, 0.6);
+        var x;
+        var y;
+        do {
+            x = 4 + Math.ceil(Math.random() * 8) - 1;
+            y = Math.ceil(Math.random() * 6) - 1;
+        } while ((x == 5 && y == 3 && this.gameboard[x][y] != null));
+        // console.log(x);
 
-		currentBori.setPosition(x, y);
-		currentBori.hpBar.setTransform(35 + 120 * x, (120 * y) + 10, 0.6, 0.6);
-		currentBori.setFaction(CombatService.TurnEnemy);
-		this.add(currentBori);
-	}
-	console.log(this.gameboard);
-	this.initialized = true;
+        currentBori.setPosition(x, y);
+        currentBori.hpBar.setTransform(35 + 120 * x, (120 * y) + 10, 0.6, 0.6);
+        currentBori.setFaction(CombatService.TurnEnemy);
+        this.add(currentBori);
+    }
+    console.log(this.gameboard);
+    this.initialized = true;
 };
 
 testLevel.prototype.update = function(event) {
-	if (this.initialized) {
+    if (this.initialized) {
 
-	}
+    }
 };
 testLevel.prototype.dispose = function() {
 
