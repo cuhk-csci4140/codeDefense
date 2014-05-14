e642bc5a63a8c994c0feffda0d2e65735718d471
Ng Ting Fung <ntf@ntflab.com>
2014-05-14 14:54:16 +0800

    balance skills cost , update tooltips

--- game/contents/spells/Thunderbolt.js
+++ game/contents/spells/Thunderbolt.js
@@ -21,7 +21,7 @@ var Thunderbolt = function(world, args) {
 	this.args = args;
 	this.castTime = 5;
 	this.damage = 5;
-	this.cost = 15;
+	this.cost = 20;
 	console.log("Thunderbolt initialized");
 	this.initialize();
 }
